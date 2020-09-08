f4b77e27bb8c4169c54c4ef6d8c7c4c369897a89
Mayowa Ojo <mayowaojo.e@gmail.com>
2020-09-08 09:31:15 -0600

    :sparkles: [feat]: create editor component + logic

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,24 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+   <div id="app" class="pt-10">
+      <div class="container mx-auto w-1/2">
+         <Editor :toolbarId="generateId()"/>
+         <Editor :toolbarId="generateId()"/>
+      </div>
+   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+   import Editor from "./components/Editor.vue";
+   import shortid from "shortid";
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+   export default {
+      name: "App",
+      components: {
+         Editor,
+      },
+      methods: {
+         generateId: () => shortid.generate()
+      }
+   };
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
+import VueQuill from "vue-quill-editor";
+import 'quill/dist/quill.core.css' // import styles
+import 'quill/dist/quill.snow.css' // for snow theme
+
 import App from './App.vue'
+import "./assets/css/app.css"
 
 Vue.config.productionTip = false
 
+Vue.use(VueQuill)
+
 new Vue({
-  render: h => h(App),
+   render: h => h(App)
 }).$mount('#app')
